8331f2e12d95f942811cb1d0729e26545a315693
tsoa <henintsoapaul@gmail.com>
2024-05-29 14:45:43 +0300

    Add commit controls during delete queries.

--- src/database/query/GenericDelete.java
+++ src/database/query/GenericDelete.java
@@ -1,5 +1,6 @@
 package database.query;
 
+import database.GenericDaoException;
 import database.connector.DatabaseConnector;
 import database.utils.QueryUtil;
 
@@ -9,11 +10,39 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class GenericDelete {
-    public static int exeDelete( String query, Connection connection )
+    /**
+     *
+     * @param query The delete query to be executed.
+     * @param connection An SQL connection to the database.
+     * @param nbRowsToDeleted The estimated number of rows to be deleted by the query.
+     *  When we do not know in advance how many rows will be affected, use -1.
+     * @return The number of rows that have been deleted.
+     */
+    public static int exeDelete( String query, Connection connection, int nbRowsToDeleted )
             throws SQLException {
         try ( PreparedStatement preparedStatement = connection.prepareStatement( query ) ) {
-            return preparedStatement.executeUpdate();
+            if ( nbRowsToDeleted != -1 ) {
+                return preparedStatement.executeUpdate();
+            }
+            else {
+                connection.setAutoCommit( false );
+                int nbRows = preparedStatement.executeUpdate();
+                if ( nbRows != nbRowsToDeleted ) {
+                    throw new GenericDaoException( " An Exception occurred during DELETE. " + nbRows + " row(s) deleted\n" +
+                            ", but " + nbRowsToDeleted + " row(s) was supposed to be deleted." );
+                }
+                connection.commit();
+                return nbRows;
+            }
         }
+    }
+
+    /**
+     * Use this method when you do not know how many rows will be deleted.
+     */
+    public static int exeDelete( String query, Connection connection )
+            throws SQLException {
+        return exeDelete( query, connection, -1 );
     }
 
     public static int deleteAll( Object object, Connection connection )
@@ -32,24 +61,24 @@ public class GenericDelete {
     }
 
     // delete by id primary_key (set ColumnAnnotation.primarykey = true)
-    public static int deleteById( Object object, int idPrimaryKey, Connection connection )
+    public static int deleteById( Object object, int pkValue, Connection connection )
             throws SQLException {
         Class<?> clazz = object.getClass();
         String tableName = QueryUtil.getTableName( clazz );
         Field pkField = QueryUtil.getPrimaryKeyField( clazz );
         if ( pkField == null ) {
-            throw new SQLException( "Cannot perform delete query 'coz there is no PRIMARY KEY in the relation \"" + tableName + "\"." );
+            throw new GenericDaoException( "Cannot perform delete query 'coz there is no PRIMARY KEY in the relation \"" + tableName + "\"." );
         }
         String pkColumnName = QueryUtil.getColumnName( pkField ),
-                query = "DELETE FROM " + tableName + " WHERE " + pkColumnName + " = " + idPrimaryKey;
-        return exeDelete( query, connection );
+                query = "DELETE FROM " + tableName + " WHERE " + pkColumnName + " = " + pkValue;
+        return exeDelete( query, connection, 1 );
     }
 
-    public static int deleteById( Object object, int idPrimaryKey, DatabaseConnector databaseConnector )
+    public static int deleteById( Object object, int pkValue, DatabaseConnector databaseConnector )
             throws SQLException, ClassNotFoundException {
         int nbRows;
         try ( Connection connection = databaseConnector.getConnection() ) {
-            nbRows = deleteById( object, idPrimaryKey, connection );
+            nbRows = deleteById( object, pkValue, connection );
         }
         return nbRows;
     }
