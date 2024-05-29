b6876fcd207edc9125ff2de741a12c5a36a00f9c
tsoa <henintsoapaul@gmail.com>
2024-05-29 13:56:13 +0300

    Re-factorisation of GenericDelete

--- src/database/query/GenericDelete.java
+++ src/database/query/GenericDelete.java
@@ -24,8 +24,11 @@ public class GenericDelete {
 
     public static int deleteAll( Object object, DatabaseConnector databaseConnector )
             throws SQLException, ClassNotFoundException {
-        String query = "DELETE FROM " + Util.getTableName( object.getClass() );
-        return exeDelete( query, databaseConnector.getConnection() );
+        int nbRows;
+        try ( Connection connection = databaseConnector.getConnection() ) {
+            nbRows = deleteAll( object, connection );
+        }
+        return nbRows;
     }
 
     // delete by id primary_key (set ColumnAnnotation.primarykey = true)
@@ -33,25 +36,26 @@ public class GenericDelete {
             throws SQLException {
         Class<?> clazz = object.getClass();
         String tableName = Util.getTableName( clazz );
-
         Field pkField = Util.getPrimaryKeyField( clazz );
         if ( pkField == null ) {
             throw new SQLException( "Cannot perform delete query 'coz there is no PRIMARY KEY in the relation \"" + tableName + "\"." );
         }
-        String pkColumnName = Util.getColumnName( pkField );
-
-        String query = "DELETE FROM " + tableName + " WHERE " + pkColumnName + " = " + idPrimaryKey;
+        String pkColumnName = Util.getColumnName( pkField ),
+                query = "DELETE FROM " + tableName + " WHERE " + pkColumnName + " = " + idPrimaryKey;
         return exeDelete( query, connection );
     }
 
     public static int deleteById( Object object, int idPrimaryKey, DatabaseConnector databaseConnector )
             throws SQLException, ClassNotFoundException {
-        return deleteById( object, idPrimaryKey, databaseConnector.getConnection() );
+        int nbRows;
+        try ( Connection connection = databaseConnector.getConnection() ) {
+            nbRows = deleteById( object, idPrimaryKey, connection );
+        }
+        return nbRows;
     }
 
     // delete using a condition ==
     // delete using a condition <
     // delete using a condition >
     // delete using a condition LIKE
-
 }
