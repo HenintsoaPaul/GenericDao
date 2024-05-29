3af6c84d4713c94d92942bd99cf2b0508388acf5
tsoa <henintsoapaul@gmail.com>
2024-05-29 13:44:16 +0300

    Delete using primary key.

--- src/database/query/GenericDelete.java
+++ src/database/query/GenericDelete.java
@@ -3,6 +3,7 @@ package database.query;
 import database.connector.DatabaseConnector;
 import database.utils.Util;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -27,11 +28,25 @@ public class GenericDelete {
         return exeDelete( query, databaseConnector.getConnection() );
     }
 
-    // delete by id
-    public static int deleteById( Object object, int id, Connection connection )
+    // delete by id primary_key (set ColumnAnnotation.primarykey = true)
+    public static int deleteById( Object object, int idPrimaryKey, Connection connection )
             throws SQLException {
-        String query = "DELETE FROM " + Util.getTableName( object.getClass() ) + " WHERE id = " + id;
+        Class<?> clazz = object.getClass();
+        String tableName = Util.getTableName( clazz );
+
+        Field pkField = Util.getPrimaryKeyField( clazz );
+        if ( pkField == null ) {
+            throw new SQLException( "Cannot perform delete query 'coz there is no PRIMARY KEY in the relation \"" + tableName + "\"." );
+        }
+        String pkColumnName = Util.getColumnName( pkField );
+
+        String query = "DELETE FROM " + tableName + " WHERE " + pkColumnName + " = " + idPrimaryKey;
         return exeDelete( query, connection );
+    }
+
+    public static int deleteById( Object object, int idPrimaryKey, DatabaseConnector databaseConnector )
+            throws SQLException, ClassNotFoundException {
+        return deleteById( object, idPrimaryKey, databaseConnector.getConnection() );
     }
 
     // delete using a condition ==
